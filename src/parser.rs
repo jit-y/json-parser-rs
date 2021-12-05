51e4ecff6462c0c259facac8daee45eddf87c6d2
ytakhs <21127627+jit-y@users.noreply.github.com>
2021-12-05 16:48:23 +0900

    Refactor parse_array

--- src/parser.rs
+++ src/parser.rs
@@ -5,30 +5,23 @@ use crate::lexer::{
     Lexer,
 };
 use anyhow::{anyhow, Result};
+use std::collections::HashMap;
+use std::iter::Peekable;
 use value::Value;
 
 pub struct Parser<'c> {
-    lexer: Lexer<'c>,
-    current_token: Option<Token>,
-    next_token: Option<Token>,
+    lexer: Peekable<Lexer<'c>>,
 }
 
 impl<'c> Parser<'c> {
     pub fn new(lexer: Lexer<'c>) -> Self {
-        let mut parser = Self {
-            lexer: lexer,
-            current_token: None,
-            next_token: None,
-        };
-
-        parser.read_token();
-        parser.read_token();
-
-        parser
+        Self {
+            lexer: lexer.peekable(),
+        }
     }
 
     pub fn parse(&mut self) -> Result<Value> {
-        match &self.current_token {
+        match self.next_token() {
             None => return Err(anyhow!("err")),
             Some(tok) => match &tok.token_type {
                 TokenType::Null => self.parse_null(),
@@ -40,50 +33,46 @@ impl<'c> Parser<'c> {
         }
     }
 
-    fn read_token(&mut self) {
-        std::mem::swap(&mut self.next_token, &mut self.current_token);
-        self.next_token = self.lexer.next();
+    fn next_token(&mut self) -> Option<Token> {
+        self.lexer.next()
+    }
+
+    fn peek_token(&mut self) -> Option<&Token> {
+        self.lexer.peek()
     }
 
     fn parse_null(&mut self) -> Result<Value> {
-        self.read_token();
         Ok(Value::Null)
     }
 
     fn parse_string(&mut self, val: String) -> Result<Value> {
-        self.read_token();
         Ok(Value::String(val))
     }
 
     fn parse_number(&mut self, val: f64) -> Result<Value> {
-        self.read_token();
         Ok(Value::Number(val))
     }
 
     fn parse_array(&mut self) -> Result<Value> {
         let mut res = vec![];
 
-        self.read_token();
-
-        while let Some(tok) = &self.current_token {
+        while let Some(tok) = self.peek_token() {
             match tok.token_type {
                 TokenType::RBracket => {
-                    self.read_token();
+                    self.next_token();
                     break;
                 }
-                TokenType::Comma => match &self.next_token {
-                    Some(tok) => {
-                        if tok.token_type == TokenType::LBracket {
-                            return Err(anyhow!("invalid"));
+                TokenType::Comma => {
+                    self.next_token();
+                    if let Some(tok) = self.peek_token() {
+                        if tok.token_type == TokenType::RBracket {
+                            return Err(anyhow!("Invalid token {}", tok.literal));
                         }
-                        self.read_token();
                     }
-                    None => return Err(anyhow!("invalid")),
-                },
+                }
                 _ => {
                     let v = self.parse()?;
                     res.push(v);
-                    self.read_token();
                 }
             }
         }
